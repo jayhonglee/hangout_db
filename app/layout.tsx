b70dec23daa7a6225b01a87ab33ce1596f8b1866
Jaehong Lee <ja34luv@gmail.com>
2025-03-22 20:44:33 -0700

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Hangout",
   description: "Create or join hangouts with your friends",
+  icons: {
+    icon: [
+      {
+        url: "/favicon.svg",
+        type: "image/svg+xml",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -16,6 +24,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
